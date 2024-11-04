30c07707db638edc003cc0f0c08c5bbcc82d1704
sam12414 <odusanyasamuel13@gmail.com>
2024-11-04 13:37:02 +0100

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,8 @@
+print ("Hello World")
+echo "# -hello-world" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/sam12414/-hello-world.git
+git push -u origin main
